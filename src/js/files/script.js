ca3446b2457c99e99159cf9f7b122541a4e5e468
Yuri Matvieiev <matvssv.yura@gmail.com>
2023-08-17 19:44:49 +0300

    fix popupv2

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -162,31 +162,3 @@ if (gameRefresh) {
     gameIframe.src = gameIframe.src; // Refresh the iframe by resetting its source
   });
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  var popup = document.querySelector(".popup");
-  var popupButton = document.querySelector(".popup__button");
-
-  if (popup && isPopupVisible) {
-    // Check if the user has already confirmed their age
-    var ageConfirmed = localStorage.getItem("ageConfirmed");
-
-    if (!ageConfirmed || !isConfirmationCachingEnabled) {
-      // Add the classes to show the popup and lock scrolling
-      document.documentElement.classList.add("popup-open");
-      popup.style.display = "flex";
-    }
-
-    popupButton.addEventListener("click", function () {
-      // Remove the classes to hide the popup and unlock scrolling
-      document.documentElement.classList.remove("popup-open");
-      popup.style.display = "none";
-      if (isConfirmationCachingEnabled) {
-        localStorage.setItem("ageConfirmed", "true");
-      }
-    });
-  }
-});
-
-var isPopupVisible = true; // Set to true to show the popup by default
-var isConfirmationCachingEnabled = true; // Set to true to enable caching
